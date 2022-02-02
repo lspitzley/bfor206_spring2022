2e98e4a96b57eaa9d51b236991f2466a460f5717
Lee Spitzley <lspitzley@albany.edu>
2022-02-02 13:05:23 -0500

    Code for 2-2 variables & arguments

---
+++ mon_wed/args_script.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+# This script will show how arguments work
+
+echo "Show the number of arguments"
+echo $#
+
+# Show the first argument
+echo "The first argument is: $1"
+
+#Show the 0 argument
+echo "The 0th argument is: $0"
+
+echo "Argument 2: $2"
+echo "Argument 3: $3"
+
+
+echo "Done."
