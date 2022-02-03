2403b063d66b93e3f0a479d56073579a16eb0846
Lee Spitzley <lspitzley@albany.edu>
2022-02-03 17:40:11 -0500

    added description to Tuesday's script

--- tue_thu/echo_lab.sh
+++ tue_thu/echo_lab.sh
@@ -1,5 +1,10 @@
 #! /bin/bash # this is what to use to run the file
 
+#######################
+# This script prints your name and the current directory.
+# Try changing directories and running the script again.
+#######################
+
 # print out your name
 echo "Lee Spitzley"
 
