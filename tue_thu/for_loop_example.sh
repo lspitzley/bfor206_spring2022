225186414340376270f29997799694e37a2260b8
Lee Spitzley <lspitzley@albany.edu>
2022-02-10 17:18:17 -0500

    for loops for 3-2

---
+++ tue_thu/for_loop_example.sh
@@ -0,0 +1,50 @@
+#! /bin/bash
+
+# examples of different for loop control statements
+
+# usage: Argument between 1 and 4 to run a specific loop
+# example: bash for_loop_example.sh 3 
+
+# make sure there is an argument
+if [ $# -ne 1 ]
+then
+	echo "Need exactly 1 argument. Quitting"
+	exit
+fi
+
+
+if [ $1 -eq 1 ]
+then
+	for i in {0..5}
+	do
+		echo "The value of \$i is $i"
+	done
+
+elif [ $1 -eq 2 ]
+then
+	name="Lee A Spitzley"
+	for part in $name
+	do
+		echo "$part"
+	done
+
+elif [ $1 -eq 3 ]
+then
+	for n in {0..10..2}
+	do
+		
+		echo "The value in \$sum is $sum" 
+		echo "The value of \$n is $n"
+		# let "sum=$sum + n" # this is one way
+		sum=$(( $sum + n ))
+		echo "After adding \$n, the sum is $sum"
+	done
+	
+elif [ $1 -eq 4 ]
+then 
+	N=10
+	for (( i=0; i<N; i++ ))
+	do
+		echo "The value of \$i is $i"
+	done
+fi
