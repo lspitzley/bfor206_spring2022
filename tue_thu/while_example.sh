e6d10895e914493103fe09e58bcc3f59c03b68a1
Lee Spitzley <lspitzley@albany.edu>
2022-02-15 17:10:37 -0500

    while loops example

---
+++ tue_thu/while_example.sh
@@ -0,0 +1,42 @@
+#! /bin/bash
+
+
+# this script shows some while loop examples.
+
+
+#  define LIMIT here, otherwise it would have to 
+#+ be defined in each if or elif statement.
+
+LIMIT=10
+
+if [ $1 -eq 1 ]
+then
+	counter=0
+	# LIMIT=10  --> moved this outside the if statement
+	while [ $counter -le $LIMIT ]
+	do 
+		counter=$(( $counter + 1 ))
+		echo "counter is $counter"
+	  	# counter=$(( $counter + 1 ))
+	done
+
+elif [ $1 -eq 2 ]
+then
+	a=0
+	
+	# These two statements were used to debug, uncomment to use them again.
+	# echo "a is $a"
+	# echo "LIMIT is $LIMIT"
+	while [ $a -le $LIMIT ]
+	do
+		let "a=$a+1"
+		if [ $a -eq 3 ] || [ $a -eq 11 ]
+			then continue	# continue takes us to the start of the while
+		fi
+
+		echo -n "$a "
+	done
+fi
+
+echo
+echo "Done."
