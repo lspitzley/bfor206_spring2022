f782283ea5c07eb77aca93ed209422ee93e90711
Lee Spitzley <lspitzley@albany.edu>
2022-04-18 13:08:42 -0400

    implement function to check for date row

--- mon_wed/irc_cleaner.py
+++ mon_wed/irc_cleaner.py
@@ -5,3 +5,22 @@ and produce a clean dataframe that is
 suitable for analysis in a Jupyter Notebook.
 
 """
+#%% imports
+import pandas as pd
+
+
+# import our functions
+import irc_parse
+
+#%% read in data
+raw_log = []
+with open('../data/hackers.log', 'r', errors='ignore') as log_file:
+    raw_log = log_file.readlines()
+
+#%% create dataframe
+hackers = pd.DataFrame(raw_log, columns=['original_data'])
+
+
+#%% use the is_date_row function
+# check if a row is actually a date.
+hackers['is_date_row'] = hackers['original_data'].apply(irc_parse.is_date_row)
