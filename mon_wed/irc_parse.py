47838a447d72182cda2b49ff54af4d3b300e1ba0
Lee Spitzley <lspitzley@albany.edu>
2022-04-18 13:08:06 -0400

    check is_date_row and sanity check working

--- mon_wed/irc_parse.py
+++ mon_wed/irc_parse.py
@@ -7,6 +7,13 @@ IRC chat files.
 #%% imports
 import re
 
+#%% test to make sure pytest works
+def sanity_check():
+    """This test should always pass.
+    The purpose is to make sure Github actions are functioning properly.
+    """
+    return True
+
 
 #%% function to find hours and minutes
 
@@ -17,12 +24,31 @@ def get_hours_minutes(row):
 
 
 #%% define some test cases
+def test_get_hours_minutes():
+    date_changed_row = '--- Log opened Tue Sep 20 00:01:49 2016'
+    join_quit_row = '00:01 -!- Guest40341 [AndChat2541@AN-pl0gl1.8e2d.64f9.r226rd.IP] has quit [Quit: Bye]'
+    message_row = '00:25 < ice231> anyone good with exploiting cisco asa with extrabacon?'
+    
 
-date_changed_row = '--- Log opened Tue Sep 20 00:01:49 2016'
-join_quit_row = '00:01 -!- Guest40341 [AndChat2541@AN-pl0gl1.8e2d.64f9.r226rd.IP] has quit [Quit: Bye]'
-message_row = '00:25 < ice231> anyone good with exploiting cisco asa with extrabacon?'
+    assert get_hours_minutes(date_changed_row) == {}
+    assert get_hours_minutes(join_quit_row) == {'hour': 0, 'minute': 1}
+    assert get_hours_minutes(message_row) == {'hour': 0, 'minute': 25}
 
-#%% write simple tests
-assert get_hours_minutes(date_changed_row) == {}
-assert get_hours_minutes(join_quit_row) == {'hour': 0, 'minute': 1}
-assert get_hours_minutes(message_row) == {'hour': 0, 'minute': 25}
+
+
+
+
+
+def is_date_row(row):
+    
+    if re.search(r'---', row):
+        return True
+    return False
+    
+
+
+
+
+
+
+
